460c2ecbc349b96ee0f0d7aa63184a8679df79fd
rrajali <rrajali@paypal.com>
2016-03-07 10:08:33 -0800

    Mon Mar  7 10:08:33 PST 2016

--- src/root.js
+++ src/root.js
@@ -1,1 +1,21 @@
-document.write('hello fluent')
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import App from './app';
+import './styles/main.css';
+
+render(<App/>, document.body.firstChild)
+
+//const Hello = React.createClass({
+//
+//    render () {
+//        return <div>Hello, FLuent poeple </div>
+//    }
+//})
+//render(<Hello/>, document.body)
+//document.write('hello fluent')
+
+//const Hello =({name})=> {
+//    return <div>Hello, FLUENT people {name}</div>
+//}
+//
+//render (<Hello name='Reefath'/>, document.body)
